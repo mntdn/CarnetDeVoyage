4a1b97691d50a1f97adcf49b295d9964713fefc3
mntdn <matmon@gmail.com>
2014-08-13 00:25:36 +0200

    Chargement fichier

--- voyage.js
+++ voyage.js
@@ -1,7 +1,7 @@
 angular.module('voyage', [])
-    .controller('ArticleController', ['$scope', function($scope) {
-        $scope.contenu = '';
-        $scope.loadArticle = function() {
-            $scope.contenu = 'Chargé !';
+    .controller('ArticleController', ['$scope', function ($scope) {
+        $scope.articleCourant = '';
+        $scope.loadArticle = function (i) {
+            $scope.articleCourant = 'voyage1.html';
         };
     }]);
