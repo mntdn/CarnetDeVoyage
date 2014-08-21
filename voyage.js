9c47f4246bc87c3cfe8441481c8f0bb27206d8e5
mntdn <matmon@gmail.com>
2014-08-21 08:18:58 +0200

    Structure
    Début de "base de données" pour génération automatique du menu

--- voyage.js
+++ voyage.js
@@ -1,7 +1,19 @@
 angular.module('voyage', [])
     .controller('ArticleController', ['$scope', function ($scope) {
+        $scope.DB = [
+            {
+                Day : '2008-07-11',
+                Pictures : ['Mandarin_Pair.jpg', 'Glassy_carbon_and_a_1cm3_graphite_cube_HP68-79.jpg'],
+                Content : 'voyage1.html'
+            },
+            {
+                Day : '2008-07-12',
+                Pictures : ['1280px-Various_grains.jpg', 'NASA-HS201427a-HubbleUltraDeepField2014-20140603.jpg'],
+                Content : 'voyage2.html'
+            }
+        ];
         $scope.articleCourant = '';
         $scope.loadArticle = function (i) {
-            $scope.articleCourant = 'voyage1.html';
+            $scope.articleCourant = $scope.DB[i].Content;
         };
     }]);
