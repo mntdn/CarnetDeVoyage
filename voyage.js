80281363a23111122d50b99285c0b0f613f04615
Matthieu M <matmon@gmail.com>
2014-08-21 18:36:18 +0200

    Images dynamiques

--- voyage.js
+++ voyage.js
@@ -1,19 +1,32 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 angular.module('voyage', [])
     .controller('ArticleController', ['$scope', function ($scope) {
         $scope.DB = [
             {
                 Day : '2008-07-11',
-                Pictures : ['Mandarin_Pair.jpg', 'Glassy_carbon_and_a_1cm3_graphite_cube_HP68-79.jpg'],
+                Pictures : [
+                    {Url : 'Mandarin_Pair.jpg'}, 
+                    {Url : 'Glassy_carbon_and_a_1cm3_graphite_cube_HP68-79.jpg'}
+                ],
                 Content : 'voyage1.html'
             },
             {
                 Day : '2008-07-12',
-                Pictures : ['1280px-Various_grains.jpg', 'NASA-HS201427a-HubbleUltraDeepField2014-20140603.jpg'],
+                Pictures : [
+                    {Url : '1280px-Various_grains.jpg'}, 
+                    {Url : 'NASA-HS201427a-HubbleUltraDeepField2014-20140603.jpg'}
+                ],
                 Content : 'voyage2.html'
             }
         ];
         $scope.articleCourant = '';
         $scope.loadArticle = function (i) {
             $scope.articleCourant = $scope.DB[i].Content;
+        };
+        $scope.randPicture = function (picList) {
+            return picList[getRandomInt(0, picList.length)].Url;
         };
     }]);
