b0b148e5d1d41b3096d19696304fe5e096043d03
Matthieu M <matmon@gmail.com>
2014-08-26 17:28:18 +0200

    Calendrier !
    Le calendrier commence à ressembler à quelque chose !

--- voyage.js
+++ voyage.js
@@ -2,19 +2,31 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// renvoie une date au format YYYY-MM-DD
+function dateToString(d) {
+    return d.getFullYear() + '-' + ('0'+(d.getMonth()+1)).slice(-2) + '-' + ('0'+(d.getDate())).slice(-2);
+}
+
+// compare 2 dates sans tenir compte de l'heure
+function isSameDate(d1, d2) {
+    return (d1.getFullYear() === d2.getFullYear()) && 
+        (d1.getMonth() === d2.getMonth()) &&
+        (d1.getDate() === d2.getDate());
+}
+
 // renvoie l'index de l'entrée de la base DB du jour cherché
 // day doit être au format Date
 // -1 s'il ne trouve rien
 function dayIndex(day, DB) {
     for(var i = 0; i < DB.length; i++)
-        if (new Date(DB[i].Day) === day)
+        if (isSameDate(new Date(DB[i].Day), day))
             return i;
     return -1;
 }
 
+// renvoie un tableau contenant les jours des mois
+// durant lesquels on a voyagé
 function calcMonths (DB) {
-    // renvoie un tableau contenant les jours des mois
-    // durant lesquels on a voyagé
     var startDate = new Date(DB[0].Day);
     var endDate = new Date(DB[DB.length-1].Day);
     var currentDate = startDate;
@@ -24,12 +36,11 @@ function calcMonths (DB) {
         var nbDaysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
         var currentDays = new Array();
         for (var i = 1; i <= nbDaysInMonth; i++) {
-            var tempDay = {
-                'Day' : tempCurrentDate,
-                'PositionDB' : dayIndex(tempCurrentDate, DB)
-            };
-            currentDays.push(tempDay);
-            console.log(currentDays);
+            currentDays.push({
+                'Day' : dateToString(tempCurrentDate),
+                'PositionDB' : dayIndex(tempCurrentDate, DB),
+                'DayOfWeek' : tempCurrentDate.getDay() == 0 ? 7 : tempCurrentDate.getDay() // lundi = 1, dimanche = 7 
+            });
             tempCurrentDate.setDate(tempCurrentDate.getDate() + 1);
         }
         returnArray.push(currentDays);
@@ -48,7 +59,7 @@ var _DB = [
         'Content' : 'voyage1.html'
     },
     {
-        'Day' : '2008-08-12',
+        'Day' : '2008-04-12',
         'Pictures' : [
             {'Url' : '1280px-Various_grains.jpg'}, 
             {'Url' : 'NASA-HS201427a-HubbleUltraDeepField2014-20140603.jpg'}
@@ -69,6 +80,9 @@ angular.module('voyage', [])
         $scope.DB = _DB;
         $scope.Months = _Months;
         $scope.articleCourant = '';
+        $scope.moveFirst = function (day, i) {
+            return i == 0 ? "margin-left: " + ((day.DayOfWeek - 1) * 42) + "px" : "";
+        };
         $scope.loadArticle = function (i) {
             $scope.articleCourant = $scope.DB[i].Content;
         };
